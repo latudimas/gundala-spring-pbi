27a9c15899e42abc46137def59f9c873ec8443bf
latudimas <riswandhalatu.dimas@mitrais.com>
2019-02-28 13:35:26 +0800

    first commit

---
+++ src/main/java/com/mitrais/gundala/gundalapbi/GundalaPbiApplication.java
@@ -0,0 +1,13 @@
+package com.mitrais.gundala.gundalapbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GundalaPbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GundalaPbiApplication.class, args);
+	}
+
+}
